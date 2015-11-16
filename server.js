4d90ede44f493408dcbfa12b7c0bdcc5fe1f46df
RuiHanCPP <elfinhuangcat@gmail.com>
2015-11-16 02:15:53 -0500

    server.js modified

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+require('./public/assignment/server/app.js')(app, null, null);
+
 app.listen(port, ipaddress);
